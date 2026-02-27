0961bf95f7002b267a4fe7cd74e0b110238c57f1
nanaios <game.ryd.131@gmail.com>
2026-02-28 00:19:12 +0900

    まずは綺麗にする

---
+++ src/main/java/com/nanaios/polygonal_tech/PolygonalTech.java
@@ -0,0 +1,15 @@
+package com.nanaios.polygonal_tech;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(PolygonalTech.MODID)
+public class PolygonalTech {
+
+    public static final String MODID = "polygonal_tech";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public PolygonalTech() {
+    }
+}
