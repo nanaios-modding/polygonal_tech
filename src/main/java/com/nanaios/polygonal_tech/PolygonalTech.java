aad22dc771a94d3ba26fc4f958f049fc4cb9b248
nanaios <game.ryd.131@gmail.com>
2026-03-17 17:02:13 +0900

    AI、俺とトントンやん

--- src/main/java/com/nanaios/polygonal_tech/PolygonalTech.java
+++ src/main/java/com/nanaios/polygonal_tech/PolygonalTech.java
@@ -4,7 +4,6 @@ import com.mojang.logging.LogUtils;
 import com.nanaios.polygonal_tech.config.PolygonalTechConfig;
 import com.nanaios.polygonal_tech.network.PolygonalTechNetwork;
 import com.nanaios.polygonal_tech.registries.*;
-import com.nanaios.polygonal_tech.launch.SyncFieldRegistry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -30,11 +29,6 @@ public class PolygonalTech {
         PolygonalTechBlockEntityTypeRegister.BLOCK_ENTITIES.register(bus);
         PolygonalTechCreativeModeTabRegister.CREATIVE_TABS.register(bus);
         PolygonalTechMenuTypeRegister.MENU_TYPES.register(bus);
-
-        LOGGER.info("log all sync field");
-        for(var fields: SyncFieldRegistry.getAll().entrySet()) {
-            LOGGER.info("class: {}, fields: {}", fields.getKey(), fields.getValue());
-        }
 
         // commonSetupを登録
         bus.addListener(this::commonSetup);
