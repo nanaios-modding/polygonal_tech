743130cabefb2b98e2eec62f6c9cd562f2bdc359
nanaios <game.ryd.131@gmail.com>
2026-03-20 22:48:31 +0900

    DataGenをいじいじ

--- src/main/java/com/nanaios/polygonal_tech/datagen/PolygonalTechLanguageProvider.java
+++ src/main/java/com/nanaios/polygonal_tech/datagen/PolygonalTechLanguageProvider.java
@@ -4,8 +4,12 @@ import com.nanaios.polygonal_tech.PolygonalTech;
 import com.nanaios.polygonal_tech.lang.ILang;
 import com.nanaios.polygonal_tech.lang.PolygonalTechToolTipLang;
 import com.nanaios.polygonal_tech.registries.PolygonalTechBlockRegister;
+import com.nanaios.polygonal_tech.registries.PolygonalTechFluidTypeRegister;
+import net.minecraft.Util;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.LanguageProvider;
+import net.minecraftforge.fluids.FluidType;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class PolygonalTechLanguageProvider extends LanguageProvider {
     public PolygonalTechLanguageProvider(PackOutput output) {
@@ -15,9 +19,14 @@ public class PolygonalTechLanguageProvider extends LanguageProvider {
     @Override
     protected void addTranslations() {
         add(PolygonalTechBlockRegister.PHOTOLYSIS_MACHINE_MK1.get(), "光分解機MK.1");
-        add("fluid_type.polygonal_tech.third_flow", "3次流元");
+        add(PolygonalTechFluidTypeRegister.THIRD_FLOW_TYPE.get(), "3次流元");
         add(PolygonalTechToolTipLang.FLUID_TANK_EMPTY, "空");
         add(PolygonalTechToolTipLang.FLUID_TANK_AMOUNT, "%s / %s mB");
+    }
+
+    private void add(FluidType type, String translation) {
+        String descriptionId = Util.makeDescriptionId("fluid_type", ForgeRegistries.FLUID_TYPES.get().getKey(type));
+        add(descriptionId, translation);
     }
 
     private void add(ILang lang, String translation) {
